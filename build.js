f87c498022b6f3c9bea5b7cef5b6f76c27c3e86a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-30 11:12:27 -0700

    asdf

---
+++ build.js
@@ -0,0 +1,16 @@
+import { copyFile, mkdir, readFile, writeFile } from "node:fs/promises";
+import { build } from "esbuild";
+import { execSync } from "node:child_process";
+
+await mkdir("dist");
+
+let builder = await build({
+	platform: "browser",
+	sourcemap: true,
+	minify: false,
+  entryPoints: ['index.js'],
+	bundle: true,
+	treeShaking: true,
+	logLevel: "info",
+	outdir: "dist/",
+});
---
+++ index.js
@@ -0,0 +1,9 @@
+/// As a proxy developer
+import { BareClient } from "@mercuryworkshop/bare-mux";
+
+
+const client = new BareClient();
+
+
+  // Fetch
+const resp =  client.fetch("https://example.com");
