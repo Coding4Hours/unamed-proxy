ac7e487c9f202d344934cd5ee484c0514b050916
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-30 12:21:28 -0700

    Update build.js

--- build.js
+++ build.js
@@ -5,12 +5,12 @@ import { execSync } from "node:child_process";
 await mkdir("dist");
 
 let builder = await build({
-	platform: "browser",
-	sourcemap: true,
-	minify: false,
+  platform: "browser",
+  sourcemap: true,
+  minify: false,
   entryPoints: ['src/index.js'],
-	bundle: true,
-	treeShaking: true,
-	logLevel: "info",
-	outdir: "dist/",
+  bundle: true,
+  treeShaking: true,
+  logLevel: "info",
+  outdir: "dist/",
 });
